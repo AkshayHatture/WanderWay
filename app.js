6750650dd3801a93d7471cda0edb0826f41c52ae
Akshay Hatture <akshayhatture11@gmail.com>
2024-10-14 13:07:40 +0530

    updated commit

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const Listing= require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-app.use(express.static(path.join(__dirname,"/public")));//public folder madhla static files use karnyasathi
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review= require("./models/review.js");
+
 
 
 app.set("view engine","ejs");
@@ -13,6 +17,7 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 main()
 .then(()=>{
@@ -30,6 +35,26 @@ app.get("/", (req, res) => {
     res.send("hi,im root");
 });
 
+const validateListing = (req,res,next) =>{
+    let {error} =listingSchema.validate(req.body)
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    }else{
+        next();
+    }
+};
+
+
+const validateReview = (req,res,next) =>{
+    let {error} =reviewSchema.validate(req.body)
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    }else{
+        next();
+    }
+};
 //
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
@@ -47,10 +72,10 @@ app.get("/", (req, res) => {
 // })
 
 //INDEX ROUTE
-app.get("/listings",async (req,res) =>{
+app.get("/listings",wrapAsync(async (req,res) =>{
       const allListings = await Listing.find({});
       res.render("listings/index.ejs",{allListings});
-});
+}));
 
 //NEW ROUTE
 app.get("/listings/new",(req,res)=>{
@@ -60,41 +85,93 @@ app.get("/listings/new",(req,res)=>{
 
 
 //SHOW ROUTE
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing});
-});
+}));
 
 //CREATE ROUTE
-app.post("/listings",async (req,res)=>{
-   const newlisting = new Listing(req.body.listing);
-   await newlisting.save();
-   res.redirect("/listings");
-});
+app.post("/listings",
+    validateListing,
+    wrapAsync(async(req,res)=>{
+        const newlisting = new Listing(req.body.listing);
+        await newlisting.save();
+        res.redirect("/listings");
+}));
 
 //EDIT ROUTE
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //UPDATE ROUTE
-app.put("/listings/:id",async (req,res)=>{
+app.put("/listings/:id",
+    validateListing,
+    wrapAsync(async (req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //DELETE ROUTE
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     let deletedListing =await Listing.findByIdAndDelete(id);
     res.redirect("/listings")
     console.log(deletedListing)
+}));
+
+
+
+//REVIEWS
+
+//Post Review Route
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    let aw = await newReview.save();
+    let aw2 =await listing.save();
+   
+    res.redirect(`/listings/${listing._id}`)
+}));
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}= req.params;
+    Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+//error-middlewares
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
 });
 
+app.use((err,req,res,next)=>{
+    let {statusCode =500, message="Something went wrong!"} = err;
+    //res.status(statuscode).send(message);
+    res.status(statusCode).render("error.ejs",{err})
+});
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const  Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -8,13 +9,27 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        filename: { type: String, /*required: true*/ },
-        url: { type: String, /*required: true, */default: 'https://unsplash.com/photos/a-river-running-through-a-forest-filled-with-lots-of-trees-Tji1V-79Qqc' }
+        type: String, /*required: true, */
+        default: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
     price: Number,
     location: String,
     country: String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review"
+        }
+    ]
 });
+
+
+listingSchema.post("findOneAndDelete",async (listing)=>{
+    if(listing){ 
+        await Review.deleteMany({_id :{$in :listing.reviews}})
+    }
+   
+})
 
 
 const Listing = mongoose.model("Listing",listingSchema);
